fe6badc8a7e273a4f06e18a840fc28aa47e81b3f
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-04 10:37:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ http.onload = function () {
 };
 //http.open('GET', 'http://router.local/login.php/');
 //"username=root&pin=admin¬Robot=on"
-http.send("username=admin&pin=password");
+http.send("username=admin&pin=secured");
